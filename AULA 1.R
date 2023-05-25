83ed9c5c948550aa501712ffd2c3dc89aab3d6c5
Otavio Augusto <otavioaugust@gmail.com>
2023-05-25 10:09:15 -0300

    Update AULA 1.R

--- AULA 1.R
+++ AULA 1.R
@@ -64,17 +64,17 @@ y <- 2
 z <- x/y
 z
 
-resto <- x %% y
+resto = x %% y
 resto
 
-quociente <- x %/% y
+quociente = x %/% y
 quociente
 
-x <- 31
-y <- 6
-z <- x/y
-resto_divisao <- x %% z
-quociente_divisao <- x %/% y
+x = 31
+y = 6
+z = x/y
+resto_divisao = x %% z
+quociente_divisao = x %/% y
 
 resto_divisao
 quociente_divisao
@@ -92,8 +92,8 @@ rm(list=ls())
 #-------------------------------------------
 # >, >=, <, <=, ==, != (diferente), &, | (ou), ! (negacao)
 
-a <- 5
-b <- 10
+a = 5
+b = 10
 
 # igual
 a==b
@@ -122,7 +122,7 @@ rm(list=ls())
 
 # criar um vetor x = 10.4, 5.6, 3.1, 6.4, 21.7
 
-x <- c(10.4,5.6,3.1,6.4,21.7)
+x = c(10.4,5.6,3.1,6.4,21.7)
 x
 
 # quantas observacoes ou comprimento do vetor
@@ -130,43 +130,43 @@ nobs <- length(x)
 nobs
 
 # o vetor y = 2*x
-y <- 2*x
+y = 2*x
 y
 
 # o vetor z = y - x
-z <- y-x
+z = y-x
 z
 
 # o vetor k = y/x
 
-k <- y/x ; k
+k = y/x ; k
 
 # somar os valores de x
-a <- sum(x)
+a = sum(x)
 a
 
 # media de x
-b <- mean(x)
+b = mean(x)
 b
 
 # desvio padrao (sd) de x
-c <- sd(x)
+c = sd(x)
 c
 
 # valor minimo de x
-d <-min(x)
+d = min(x)
 d
 
 # valor maximo de x
-e <- max(x)
+e = max(x)
 e
 
 # valor da mediana de x
-f <-median(x)
+f = median(x)
 f
 
 # criar o vetor k = a,b,c,d,e
-k <- c("a","b","c","d","e")
+k = c("a","b","c","d","e")
 k
 
 # Qual o tipo do vetor k?
@@ -186,20 +186,20 @@ x <- c(1:10,NA)
 x
 
 # media do vetor x
-a <- mean(x)
+a = mean(x)
 a
 
 # media do vetor x sem o missing
 ?mean
-b <- mean(x,na.rm=TRUE)
+b = mean(x,na.rm=TRUE)
 b 
 
 # desvio padrao de x sem o missing
-c <- sd(x,na.rm=TRUE)
+c = sd(x,na.rm=TRUE)
 c
 
 # coeficiente de variacao = desvio padrao/media
-d <- c/b
+d = c/b
 d
 
 # remover todos os objetos
@@ -209,10 +209,10 @@ rm(list=ls())
 # Sequencia de valores
 #---------------------------------------
 # criar um vetor com valores entre 0 a 100
-x <- c(0:100)
+x = c(0:100)
 x
 
-y <- c(50:100)
+y = c(50:100)
 y
 
 # remover todos os objetos
@@ -223,16 +223,16 @@ rm(list=ls())
 #---------------------------------------
 # Criar um vetor x = 5,9,3
 
-x <- c(5,9,3)
+x = c(5,9,3)
 x
 
 
 # Criar um vetor y = 10,11,12,13,14,15
-y <- c(10:15)
+y = c(10:15)
 y
 
 # Use os vetores x e y para criar a matriz m1 (3x3)
-m1 <- array(c(x,y),dim=c(3,3))
+m1 = array(c(x,y),dim=c(3,3))
 m1
 
 m1 = array(c(x,y),dim=c(3,3))
@@ -251,19 +251,19 @@ m1[1,2]
 m1[-1,]
 
 # Criar a matriz m2 sem a primeira linha da m1
-m2 <- m1[-1,]
+m2 = m1[-1,]
 m2
 
 # Criar a matriz m3 sem a segunda coluna da m1
-m3 <- m1[,-2]
+m3 = m1[,-2]
 m3
 
 # Junte as colunas das matrizes m1 e m2 em nova matriz m4
-m4 <- cbind(m1,m2)
+m4 = cbind(m1,m2)
 m4
 
 # Junte as colunas das matrizes m1 e m3 em nova matriz m4
-m4 <- cbind(m1,m3)
+m4 = cbind(m1,m3)
 m4
 
 # remover todos os objetos
@@ -276,17 +276,17 @@ rm(list=ls())
 # vetor2 = Joao, Maria, Carlos
 # vetor3 = TRUE, FALSE, TRUE
 
-vetor1 <- c(25,59,30)
-vetor2 <- c("Joao","Maria","Carlos")
-vetor3 <- c(TRUE, FALSE, TRUE)
+vetor1 = c(25,59,30)
+vetor2 = c("Joao","Maria","Carlos")
+vetor3 = c(TRUE, FALSE, TRUE)
 
-vetor4 <- c('Joao','Maria','Carlos')
+vetor4 = c('Joao','Maria','Carlos')
 vetor4
 
 
 
 # criar uma lista dos vetores 1, 2 e 3
-lista <- list(vetor1,vetor2,vetor3)
+lista = list(vetor1,vetor2,vetor3)
 lista
 
 # vertificar o tipo da lista
@@ -329,7 +329,7 @@ saveRDS(df_lista,
 # Abrir o df_lista no formato R
 #-----------------------------------
 ?readRDS
-df <- readRDS(file='D:/Users/regin/BIOLAB/DEBORAH/CURSO_R/2023/Aula_24095-2023/df_lista.R')
+df = readRDS(file='D:/Users/regin/BIOLAB/DEBORAH/CURSO_R/2023/Aula_24095-2023/df_lista.R')
 
 # lembrete: mudar o diretorio 'D:/Users/regin/BIOLAB/DEBORAH/CURSO_R/2023/Aula_24095-2023/'
 
